b9c737b3383e6a2eda5ceadb858b756f66763686
awhite862 <whiteaf862@gmail.com>
2019-01-24 14:47:25 -0800

    Add example with Diamond primitive cell

---
+++ examples/Diamond_ft_ccsd.py
@@ -0,0 +1,31 @@
+from kelvin.ccsd import ccsd
+from kelvin.scf_system import scf_system
+from pyscf.pbc import gto, scf, dft
+
+T = 0.1
+mu = 0.11
+cell = gto.Cell()
+cell.a = '''
+0.000000000, 3.370137329, 3.370137329
+3.370137329, 0.000000000, 3.370137329
+3.370137329, 3.370137329, 0.000000000'''
+cell.atom='''
+C 0.000000000000   0.000000000000   0.000000000000
+C 1.685068664391   1.685068664391   1.685068664391
+'''
+cell.basis = 'gth-szv'
+cell.pseudo = 'gth-pade'
+cell.verbose = 4
+cell.build()
+
+mf = scf.RHF(cell,exxdiv=None)
+mf.conv_tol_grad = 1e-8
+mf.conv_tol = 1e-12
+Escf = mf.kernel()
+sys = scf_system(mf,T,mu)
+ccsdT = ccsd(sys,iprint=1,max_iter=100,econv=1e-11,damp=0.0,T=T,mu=mu)
+Etot,Ecc = ccsdT.run()
+ccsdT.compute_ESN()
+print('N = {}'.format(ccsdT.N))
+print('E = {}'.format(ccsdT.E))
+print('S = {}'.format(ccsdT.S))
