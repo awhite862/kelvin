6140dc57b41adda0a7fb3544d3ad44672c7bb62c
awhite862 <whiteaf862@gmail.com>
2021-06-11 11:14:32 -0700

    Fix error in clone_deps.sh

--- .github/workflows/clone_deps.sh
+++ .github/workflows/clone_deps.sh
@@ -2,4 +2,4 @@
 git clone https://github.com/awhite862/cqcpy cqcpy_src
 git clone https://github.com/awhite862/lattice lattice_src
 ln -s cqcpy_src/cqcpy cqcpy
-ln -s lattice_src/cqcpy lattice 
+ln -s lattice_src/lattice lattice 
