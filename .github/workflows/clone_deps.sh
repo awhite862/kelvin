b99020d0ac414f4a40c748da6bd486664d1c6821
awhite862 <whiteaf862@gmail.com>
2021-06-11 11:01:17 -0700

    First push of github action

---
+++ .github/workflows/clone_deps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+git clone https://github.com/awhite862/cqcpy cqcpy_src
+git clone https://github.com/awhite862/lattice lattice_src
+ln -s cqcpy_src/cqcpy cqcpy
+ln -s lattice_src/cqcpy lattice 
---
+++ check_deps.py
@@ -0,0 +1,8 @@
+try:
+    import lattice
+except ImportError:
+    print("Failed to import 'lattice' library")  
+try:
+    import cqcpy
+except ImportError:
+    print("Failed to import 'cqcpy' library")  
